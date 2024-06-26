4d1859aa85302e8916478f5298ee99644623758c
PS Prashant <98733470+PS-PrashantFSD@users.noreply.github.com>
2024-06-26 19:41:09 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'jdnewshub.firebaseapp.com',
-  projectId: 'jdnewshub',
-  storageBucket: 'jdnewshub.appspot.com',
-  messagingSenderId: '767534919866',
-  appId: '1:767534919866:web:09428d8dee5c9d0df79283',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'mern-blog-b327f.firebaseapp.com',
+  projectId: 'mern-blog-b327f',
+  storageBucket: 'mern-blog-b327f.appspot.com',
+  messagingSenderId: '699397991367',
+  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
 };
 
 // Initialize Firebase
