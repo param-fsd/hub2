242beb094f7e7ab24a82a60fe81b70efe0e76bff
PS Prashant <98733470+PS-PrashantFSD@users.noreply.github.com>
2024-06-26 19:42:05 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,11 +7,11 @@ import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+   authDomain: 'jdnewshub.firebaseapp.com',
+  projectId: 'jdnewshub',
+  storageBucket: 'jdnewshub.appspot.com',
+  messagingSenderId: '767534919866',
+  appId: '1:767534919866:web:09428d8dee5c9d0df79283',
 };
 
 // Initialize Firebase
