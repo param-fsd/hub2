d3b792121cf9b21b101c7841368309afdb8d33f0
PS Prashant <98733470+PS-PrashantFSD@users.noreply.github.com>
2024-07-17 09:41:12 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,42 +18,42 @@ export default function Home() {
 
   return (
     <div className='bg-gray-50 dark:bg-gray-900 min-h-screen'>
-      <div className='flex flex-col gap-8 p-10 lg:p-20 px-6 max-w-7xl mx-auto'>
-      <Categories />
-        <h1 className='text-4xl font-bold lg:text-5xl text-gray-800 dark:text-white'>
+      <div className='flex flex-col gap-0 p-6 lg:p-12 px-4 max-w-7xl mx-auto'>
+        <Categories />
+        <h1 className='text-4xl font-bold lg:text-5xl text-gray-800 dark:text-white mt-4'>
           Welcome to JD News Hub.
         </h1>
-        <p className='text-gray-600 dark:text-gray-300 text-base lg:text-lg'>
+        <p className='text-gray-600 dark:text-gray-300 text-base lg:text-lg mt-2'>
           Stay updated with the latest in tech, sports, fashion, politics, and more. 
           Discover in-depth articles and breaking news on the topics that matter to you. 
           Join our community of readers and stay informed.
         </p>
         <Link
           to='/search'
-          className='text-base lg:text-lg text-blue-500 font-semibold hover:underline'
+          className='text-base lg:text-lg text-blue-500 font-semibold hover:underline mt-2'
         >
           View all posts
         </Link>
       </div>
 
-      <div className='p-4 bg-green-100 dark:bg-green-800'>
+      <div className='p-3 bg-green-100 dark:bg-green-800'>
         <CallToAction />
       </div>
 
-      <div className='max-w-7xl mx-auto p-6 flex flex-col gap-10 py-10'>
+      <div className='max-w-7xl mx-auto p-4 flex flex-col gap-8 py-8'>
         {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-8'>
+          <div className='flex flex-col gap-6'>
             <h2 className='text-3xl font-semibold text-center text-gray-800 dark:text-white'>
               Recent Posts
             </h2>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
             </div>
             <Link
               to={'/search'}
-              className='text-xl text-blue-500 hover:underline text-center'
+              className='text-xl text-blue-500 hover:underline text-center mt-4'
             >
               View all posts
             </Link>
