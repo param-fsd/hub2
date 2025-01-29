045542abb1d35ae89b9d213bc3fdae426df50176
param <98733470+param-fsd@users.noreply.github.com>
2025-01-29 22:40:30 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,13 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-   authDomain: 'jdnewshub.firebaseapp.com',
+  apiKey: 'AIzaSyDrh_cf4vZS99krRTTW1FlFbmVXJoCAfbY',
+  authDomain: 'jdnewshub.firebaseapp.com',
   projectId: 'jdnewshub',
   storageBucket: 'jdnewshub.appspot.com',
   messagingSenderId: '767534919866',
